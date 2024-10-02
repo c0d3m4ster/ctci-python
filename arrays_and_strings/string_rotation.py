016be1cf7a0b28b26f297238c75b0f33316032dc
Akbar Banitalebi <banitalebi.akbar@gmail.com>
2024-10-02 23:38:14 +0330

    string rotation
    string rotation
    
    Co-Authored-By: c0d3m4ster <182943057+c0d3m4ster@users.noreply.github.com>

---
+++ arrays_and_strings/string_rotation.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+# String Rotation:
+# Assume you have a method isSubstring which checks if one word is a substring
+# of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
+# call to isSubstring (e.g.,"waterbottle" is a rotation of "erbottlewat").
+
+def is_substring(s1, s2):
+    return s2 in s1
+
+def is_rotation(s1, s2):    
+    if len(s1) != len(s2) or len(s1) == 0:
+        return False    
+    
+    concat = s1 + s1    
+    return is_substring(concat, s2)
+
+
+
+class TestIsRotation(unittest.TestCase):
+    def test_empty_matrix(self):
+        s1 = 'waterbottle'
+        s2 = 'erbottlewat'
+        self.assertEqual(is_rotation(s1, s2), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
