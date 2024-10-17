29a422298ec7e99f16ef920634a99dd8cabedc8f
algo4rtisan <algo4rtisan@gmail.com>
2024-10-18 00:46:18 +0330

    some tests are added
    some tests are added

--- stacks_and_queues/stack_min.py
+++ stacks_and_queues/stack_min.py
@@ -26,3 +26,48 @@ class MinStack:
         if not self.min_stack:
             raise IndexError("min from empty stack")
         return self.min_stack[-1]
+
+class TestMinStack(unittest.TestCase):
+    
+    def setUp(self):
+        self.min_stack = MinStack()
+
+    def test_push_and_min(self):
+        self.min_stack.push(3)
+        self.assertEqual(self.min_stack.min(), 3)
+        
+        self.min_stack.push(5)
+        self.assertEqual(self.min_stack.min(), 3)
+        
+        self.min_stack.push(2)
+        self.assertEqual(self.min_stack.min(), 2)
+        
+        self.min_stack.push(1)
+        self.assertEqual(self.min_stack.min(), 1)
+        
+        self.min_stack.push(4)        
+        self.assertEqual(self.min_stack.min(), 1)
+
+    def test_pop_and_min(self):
+        self.min_stack.push(3)
+        self.min_stack.push(5)
+        self.min_stack.push(2)
+        
+        self.assertEqual(self.min_stack.pop(), 2)
+        self.assertEqual(self.min_stack.min(), 3)
+        
+        self.assertEqual(self.min_stack.pop(), 5)
+        self.assertEqual(self.min_stack.min(), 3)
+        
+        self.assertEqual(self.min_stack.pop(), 3)
+        
+    def test_empty_pop_and_min(self):
+        with self.assertRaises(IndexError):
+            self.min_stack.pop()                 
+            
+        with self.assertRaises(IndexError):
+            self.min_stack.min()                       
+
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
